b77070dcd5ac84318441a243c1cd43c98f23f5a7
Arun Gupta <akgupta89@gmail.com>
2019-10-23 22:41:16 -0700

    removed graphql implemented apollo-fetch

--- messages/daily-summary.js
+++ messages/daily-summary.js
@@ -1,8 +1,25 @@
+const { createApolloFetch } = require('apollo-fetch');
 
-const buses = 287;
-const runs = 897;
-const mins = 12847;
-const yesterday = new Date().toDateString("m d, Y");
+const fetch = createApolloFetch({
+    uri: 'https://muni.opentransit.city/api/graphql',
+});
+
+let buses = 287;
+let runs = 897;
+let mins = 12847;
+let yesterday = new Date().toDateString("m d, Y");
+
+fetch({
+    query: `query {
+        routes {
+            id
+            title
+        }
+    }`,
+    // variables: { id: 1 },
+}).then(res => {
+    console.log(res.data);
+});
 
 module.exports = {
     message: `OpenTransit tracked ${buses} buses performing ${runs} runs for a total of ${mins} mins on the road, ${yesterday} for #SF >> muni.opentransit.city`
